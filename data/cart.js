71ed9d0285188a2773ee934dc9fa7a7d2cb3bac0
PfabyGo <bastenprince56@gmail.com>
2026-06-09 17:19:56 +0200

    save cart to local storage

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,15 @@
-export let cart = [
-  {productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2},
-  {productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e", quantity: 10}
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [
+    {productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2},
+    {productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 1}
+  ];
+}
+
+function saveToStorage () {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart (productId) {
   let matchingItem;
@@ -22,7 +30,7 @@ export function addToCart (productId) {
       quantity: 1
     });
   }
-  console.log(cart);
+  saveToStorage();
 }
 
 export function removeFromCart (productId) {
@@ -35,4 +43,6 @@ export function removeFromCart (productId) {
   });
 
   cart=newCart;
+
+  saveToStorage();
 }
